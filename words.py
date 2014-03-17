9c88ec09bb04df9e980262f4a6effc807dc9e5c2
JTL <jtlvhpublic+github2@gmail.com>
2014-03-16 22:54:53 -0500

    modified for python 3 and fixed bugs.
    split_line now returns the word list
    
    the timed for loop at the end which does the print/sleep cycle
    now iterates over the list which is returned from split_line
    
    for next time, remove print loop from split_line and untimed
    print of results
    
    also, modify timing input to truly represent words per minute
    (timing input currently represents seconds per word divided by 60)

--- words.py
+++ words.py
@@ -1,8 +1,9 @@
 import time 
 
+# modified for python 3 - raw_input changed to input, and print statements arguments now in parenthesis
 #the text user puts in
-text = raw_input("Enter things here")
-wpm = int(raw_input("How fast would you like to read?"))
+text = input("Enter things here")
+wpm = int(input("How fast would you like to read?"))
 
 #seconds between each list item post 
 wpm_seconds = wpm/60
@@ -12,10 +13,11 @@ def split_line(text):
     words = text.split()
     for current_word in words:
         print(current_word)
+    return words # returns the words list to the calling program
         
-print split_line(text) #prints result
+print(split_line(text)) #prints result
 
 #prints items per second time.sleep(seconds between posts)
-for text in split_line:
-	print text
+for word in split_line(text): # iterates over the words in the list one word at a time
+	print(word)
 	time.sleep(wpm_seconds)
